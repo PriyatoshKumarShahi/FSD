43bc1c0d7ac0a19bbdfb8326fb4b08c123940b83
PriyatoshKumarShahi <priytoshshahi90@gmail.com>
2026-02-05 12:28:33 +0530

    Adding js

---
+++ script.js
@@ -0,0 +1,26 @@
+var a = 5;
+console.log("Hello" + a);
+
+//If else
+let age = 18;
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}     
+
+//switch
+let day = 3;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Another day");
+}
