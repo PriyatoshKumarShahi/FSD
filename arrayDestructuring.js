1aec49c8a8f7eb0046f44570e1cade74574816a7
PriyatoshKumarShahi <priytoshshahi90@gmail.com>
2026-02-16 09:34:46 +0530

    Arrow functions

---
+++ arrayDestructuring.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[0]); 
+console.log(arr[1]);
+
+// Destructuring assignment
+let [a, b, c, d, e] = arr;
+console.log(a);
--- functions.js
+++ functions.js
@@ -8,6 +8,8 @@
 // console.log(sum(a, b));
 
 
+//function expression : We can't call the function before it's defined. We can only call it after it's defined. This is because function expressions are not hoisted like function declarations.
+// console.log(`The sum of 5 and 10 is: ${add(5, 10)}`);
 const add = function(a, b) {
     return a + b;
 }
@@ -23,3 +25,20 @@ const msg = () => {
   console.log("Hello World!");
 }
 msg();
+
+// //function expression
+// const divide = function(a, b) {
+//     if (b === 0) {
+//         return "Cannot divide by zero";
+//     }
+//     return a / b;
+// }
+
+// const  sum = a => a;
+// console.log(sum(5));
+
+const sum = () => {console.log("This is an arrow function without parameters")};
+console.log(sum());// This will log the message and then log undefined because the function does not return anything.
+
+
+
